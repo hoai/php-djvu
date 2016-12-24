5e7c9c5dfc3b3c15c1ce4f82eb1da204eba70491
Arhitector <lovesosabitch@yandex.ru>
2016-12-24 22:22:32 +0300

    Добавлен Exception

---
+++ src/Exception.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * This file is part of the arhitector/php-djvu library.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author    Dmitry Arhitector <dmitry.arhitector@yandex.ru>
+ *
+ * @license   http://opensource.org/licenses/MIT MIT
+ * @copyright Copyright (c) 2016 Dmitry Arhitector <dmitry.arhitector@yandex.ru>
+ */
+namespace Arhitector\Djvu;
+
+use RuntimeException;
+
+/**
+ * Class Exception
+ *
+ * @package Arhitector\Djvu
+ */
+class Exception extends RuntimeException
+{
+	
+}
