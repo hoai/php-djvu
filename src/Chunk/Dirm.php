afbd457f44ac8a5eae3e34a0738e31e9ff4db722
Arhitector <lovesosabitch@yandex.ru>
2016-12-25 14:03:10 +0300

    Реализован парсинг чанка DIRM

--- src/Chunk/Dirm.php
+++ src/Chunk/Dirm.php
@@ -13,6 +13,8 @@
 namespace Arhitector\Djvu\Chunk;
 
 use Arhitector\Djvu\Document;
+use Arhitector\Djvu\Stream\BZZField;
+use Zerg\DataSet;
 use Zerg\Field\Collection;
 use Zerg\StreamInterface;
 
@@ -24,6 +26,41 @@ use Zerg\StreamInterface;
 class Dirm implements ChunkInterface
 {
 	use ChunkTrait;
+	
+	/**
+	 * @var bool
+	 */
+	protected $isBundled;
+	
+	/**
+	 * @var int
+	 */
+	protected $version = 0;
+	
+	/**
+	 * @var int
+	 */
+	protected $files = 0;
+	
+	/**
+	 * @var int[]
+	 */
+	protected $offsets = [];
+	
+	/**
+	 * @var string[]
+	 */
+	protected $names = [];
+	
+	/**
+	 * @var int[]
+	 */
+	protected $flags = [];
+	
+	/**
+	 * @var int[]
+	 */
+	protected $sizes = [];
 	
 	/**
 	 * Form constructor.
@@ -40,15 +77,64 @@ class Dirm implements ChunkInterface
 		
 		$structure = new Collection([
 			'id'      => ['string', 4],
-			'length'  => ['int', 4]
+			'length'  => ['int', 4],
+			'flags'   => [
+				'int',
+				1,
+				[
+					'formatter' => function ($value) {
+						return [
+							'is_bundled' => ($value >> 7) == 1, // B[7]
+							'version'    => $value & 127 // B[6..0]
+						];
+					}
+				]
+			],
+			'nFiles'  => ['int', 2],
+			'offsets' => [
+				'conditional',
+				'/flags/is_bundled',
+				[
+					true => ['arr', '/nFiles', ['int', 4]]
+				],
+				[
+					'default' => ['string', 0]
+				]
+			],
+			'encoded' => new BZZField('/length', [
+				'formatter' => function (StreamInterface $stream, DataSet $dataSet) {
+					return (new Collection([
+							'sizes' => ['arr', $dataSet->getValue('nFiles'), ['int', 3]],
+							'flags' => ['arr', $dataSet->getValue('nFiles'), ['int', 1]]
+						]))
+						       ->parse($stream) + ['stream' => $stream];
+				}
+			])
 		]);
 		
 		$structure = $structure->parse($this->getStream());
 		
+		for ($i = 0; $i < $structure['nFiles']; $i++)
+		{
+			$buffer = '';
+			
+			while (($byte = $structure['encoded']['stream']->read(1)) !== "\0" || empty($buffer))
+			{
+				$buffer .= $byte;
+			}
+			
+			$this->names[] = ltrim($buffer);
+		}
+		
+		$this->isBundled = $structure['flags']['is_bundled'];
+		$this->version = $structure['flags']['version'];
+		$this->files = $structure['nFiles'];
+		$this->offsets = $structure['offsets'];
+		$this->sizes = $structure['encoded']['sizes'];
+		$this->flags = $structure['encoded']['flags'];
+		
 		$this->setOffset($this->getStream()->getPosition());
 		$this->setLength($structure['length']);
-		
-		$this->getStream()->skip($structure['length']);
 	}
 	
 }
